1300d6a542308152d74b086792321cd38aa6c3b2
Bas Kager <bas@kager.io>
2018-02-07 15:50:00 +0100

    Working script with lambda functions, IFFEE, and Object Literals

---
+++ app/static/js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function () {
+  const sections = {
+    toggle: route => {
+      console.log('Toggle in sections object fired');
+      console.log(route);
+      const sections = document.getElementsByClassName('basic-section');
+      for (let i = 0; i < sections.length; i++) {
+        const sect = sections[i];
+        if (sect.id !== route) {
+          sect.classList.add('hide');
+        } else {
+          sect.classList.remove('hide');
+        }
+      }
+    }
+  };
+
+  const routes = {
+    init: () => {
+      console.log('Init in routes object fired');
+
+      window.addEventListener('hashchange', () => {
+        const route = location.hash.substring(1);
+        sections.toggle(route);
+      });
+    }
+  };
+
+  const app = {
+    init: () => {
+      console.log('Init in app object fired');
+      routes.init();
+    }
+  };
+
+  app.init();
+})();
