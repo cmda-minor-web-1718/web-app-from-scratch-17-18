497c7c7954e09afc531efe7fa4c8956aeba00a59
DesleyAalderink <desley-aalderink@hotmail.com>
2018-02-09 10:37:52 +0100

    latest fix
    my work.

---
+++ app/static/js/script.js
@@ -0,0 +1,38 @@
+console.log('global scope');
+//create local scope
+(function(){
+  console.log('local scope');
+  //initialize application
+  var app = {
+    // is een method wat je kan uitvoeren
+    init: function(){
+      console.log('app initialised')
+      routes.init()
+      }
+    }
+    //handle routes & state
+    var routes = {
+      init:function(){
+        // what's in the hash?
+        window.addEventListener("hashchange",function(event){
+        var route = location.hash;
+        sections.toggle(route)
+      })
+    }
+  }
+
+    //render / toggle sections
+    var sections = {
+    toggle:function(route){
+      console.log(route);
+      for (var i = 0; i < document.querySelectorAll("section").length; i++) {
+        document.querySelectorAll("section")[i].classList.add("none")
+      }
+      document.querySelector(route).classList.remove("none")
+    }
+  }
+
+  //start the application
+  app.init()
+
+})()
