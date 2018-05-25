22b747fafebccf05246fba86ae292b1bca624d57
DesleyAalderink <desley-aalderink@hotmail.com>
2018-05-25 12:44:50 +0200

    herkansing fixes

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -1,8 +1,19 @@
-import sections from './render.js'
-import localStorageData from './localstorage.js'
+
+var loader = {
+	 hide: function(){
+		 var loader = document.getElementById('loader');
+		 loader.classList.add('none')
+	 },
+	 show: function(){
+		 var loader = document.getElementById('loader');
+		 loader.classList.remove('none');
+	 }
+ }
 
 var api = {
+
 	call: function (apiURL) {
+		loader.show()
 		var promise = new Promise(function (resolve, reject) {
 			var request = new XMLHttpRequest();
 			request.open('GET', apiURL, true);
@@ -11,7 +22,9 @@ var api = {
 			   // Success!
 				var data = JSON.parse(request.responseText);
 				resolve(data)
+				loader.hide()
 			  } else {
+					console.log("yo")
 			   // We reached our target server, but it returned an error
 
 			  }
@@ -19,6 +32,7 @@ var api = {
 
 			request.onerror = function() {
 			 // There was a connection error of some sort
+			 document.body.innerHTML += 'Could not get the data. Are you connected to the internet?'
        reject('Could not get data from ' + apiURL + ', are you connected to the internet?')
 			};
 
@@ -26,7 +40,6 @@ var api = {
 		})
 		return promise
 	}
-
 }
 
 export default api
